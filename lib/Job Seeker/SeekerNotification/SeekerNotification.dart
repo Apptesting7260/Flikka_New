51c7c4607006c1cb5465d9dae4459a38a8f4f348
Apptesting7260 <testdemo7260@gmail.com>
2024-01-12 12:28:46 +0530

    notifications update

--- lib/Job Seeker/SeekerNotification/SeekerNotification.dart
+++ lib/Job Seeker/SeekerNotification/SeekerNotification.dart
@@ -45,7 +45,7 @@ class _Notification1PageState extends State<SeekerNotification> {
 
   @override
   void initState() {
-    seenController.notificationSeen(context, seekerProfileControllerr.viewSeekerData.value.seekerInfo?.email) ;
+    // seenController.notificationSeen(context, seekerProfileControllerr.viewSeekerData.value.seekerInfo?.email) ;
     super.initState();
   }
 
@@ -90,75 +90,117 @@ class _Notification1PageState extends State<SeekerNotification> {
               elevation: 0,
               title: Text("Notification", style: Get.theme.textTheme.displayLarge),
             ),
-            body: SmartRefresher(
-            controller: _refreshController,
-            onRefresh: _onRefresh,
-            onLoading: _onLoading,
-           child:   SeekerViewNotificationControllerInstanse
-          .viewSeekerNotificationData.value.seekerNotification
-          ?.length == null || SeekerViewNotificationControllerInstanse
-          .viewSeekerNotificationData.value.seekerNotification
-          ?.length == 0 ? Center(child: Text("No Notifications",style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Colors.white),)) :
-      SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: Get.width*.03,vertical: 8),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: Get.height * 0.01,),
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: SeekerViewNotificationControllerInstanse
-                  .viewSeekerNotificationData.value.seekerNotification
-                  ?.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 12.0),
-                  child: ListTile(
-                    onTap: () {
-                      Get.to(() =>  ViewNotificationJob( companyName: "${SeekerViewNotificationControllerInstanse
-          .viewSeekerNotificationData.value.seekerNotification?[index].companyName}",jobId: "${SeekerViewNotificationControllerInstanse.
-                      viewSeekerNotificationData.value.seekerNotification?[index].jobId}", seekerId: "${SeekerViewNotificationControllerInstanse.
-                      viewSeekerNotificationData.value.seekerNotification?[index].seekerId}",)) ;
-                    },
-                    tileColor: AppColors.textFieldFilledColor,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(17)
-                    ),
-                    leading: SizedBox( width: Get.width *.2,
-                      child: CachedNetworkImage(
-                        imageUrl: SeekerViewNotificationControllerInstanse
-                            .viewSeekerNotificationData.value
-                            .seekerNotification?[index]
-                            .jobFeatureImg ?? "",
-                        imageBuilder: (context, imageProvider) => Container(
-                          height: 90,
-                          width: 90,
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: DecorationImage(image: imageProvider,fit: BoxFit.cover)
-                          ),
-                        ),
-                        placeholder: (context, url) => const Center(child: CircularProgressIndicator(color: Colors.white,)),
-                      ),
-                    ),
-                    title: Text(SeekerViewNotificationControllerInstanse.viewSeekerNotificationData.value.seekerNotification?[index].companyName ?? "",
-                        overflow: TextOverflow.ellipsis,style: Theme.of(context).textTheme.bodySmall?.copyWith(color: AppColors.white,
-                            fontSize:SeekerViewNotificationControllerInstanse.viewSeekerNotificationData.value.seekerNotification?[index].seen == 1 ? 15 : 18)),
-                    subtitle: Text( SeekerViewNotificationControllerInstanse
-                        .viewSeekerNotificationData.value
-                        .seekerNotification?[index].description ?? "" ,
-                      style: Theme.of(context).textTheme.bodySmall?.copyWith(color: AppColors.white),),
+            body: PopScope(
+              canPop: true,
+             onPopInvoked: (didPop) {
+                if(didPop) {
+                  seenController.notificationSeen(context, seekerProfileControllerr.viewSeekerData.value.seekerInfo?.email) ;
+                }
+             },
+              child: SmartRefresher(
+                controller: _refreshController,
+                onRefresh: _onRefresh,
+                onLoading: _onLoading,
+                child: SeekerViewNotificationControllerInstanse
+                    .viewSeekerNotificationData.value.seekerNotification?.length == null ||
+                    SeekerViewNotificationControllerInstanse
+                        .viewSeekerNotificationData.value.seekerNotification?.length == 0
+                    ? Center(
+                  child: Text(
+                    "No Notifications",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(color: Colors.white),
                   ),
-                );
-              },
-
-            ),
-            SizedBox(height: Get.height * 0.01,),
-          ],
-        ),
-      ),
+                )
+                    : SingleChildScrollView(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Get.width * 0.03,
+                    vertical: 8,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: Get.height * 0.01,),
+                      ListView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: SeekerViewNotificationControllerInstanse
+                            .viewSeekerNotificationData.value.seekerNotification?.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 12.0),
+                            child: ListTile(
+                              onTap: () {
+                                Get.to(() => ViewNotificationJob(
+                                  companyName: "${SeekerViewNotificationControllerInstanse
+                                      .viewSeekerNotificationData.value.seekerNotification?[index].companyName}",
+                                  jobId: "${SeekerViewNotificationControllerInstanse
+                                      .viewSeekerNotificationData.value.seekerNotification?[index].jobId}",
+                                  seekerId: "${SeekerViewNotificationControllerInstanse
+                                      .viewSeekerNotificationData.value.seekerNotification?[index].seekerId}",
+                                ));
+                              },
+                              tileColor: AppColors.textFieldFilledColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(17),
+                              ),
+                              leading: SizedBox(
+                                width: Get.width * .2,
+                                child: CachedNetworkImage(
+                                  imageUrl: SeekerViewNotificationControllerInstanse
+                                      .viewSeekerNotificationData.value.seekerNotification?[index].jobFeatureImg ?? "",
+                                  imageBuilder: (context, imageProvider) => Container(
+                                    height: 90,
+                                    width: 90,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      image: DecorationImage(
+                                        image: imageProvider,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  placeholder: (context, url) => const Center(
+                                    child: CircularProgressIndicator(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              title: Text(
+                                SeekerViewNotificationControllerInstanse
+                                    .viewSeekerNotificationData.value.seekerNotification?[index].companyName ?? "",
+                                overflow: TextOverflow.ellipsis,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall
+                                    ?.copyWith(
+                                  color: AppColors.white,
+                                  fontSize: SeekerViewNotificationControllerInstanse
+                                      .viewSeekerNotificationData.value.seekerNotification?[index].seen == 1 ? 15 : 18,
+                                ),
+                              ),
+                              subtitle: Text(
+                                SeekerViewNotificationControllerInstanse
+                                    .viewSeekerNotificationData.value.seekerNotification?[index].description ?? "",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodySmall
+                                    ?.copyWith(color: AppColors.white),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                      SizedBox(height: Get.height * 0.01,),
+                    ],
+                  ),
+                ),
+              ),
             )
+
           );
       }
     }
